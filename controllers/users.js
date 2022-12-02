066a0f7c4179791d057caf6b0be04ed19ed3b5ef
Lina Belitova <libra28@ya.ru>
2022-12-02 17:11:12 +0300

    Rename file for auto-tests

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,36 @@
-const User = require('../models/user');
+const User = require("../models/user");
 const {
   SERVER_ERROR,
   INPUT_ERROR,
   NOT_FOUND_ERROR,
-} = require('../utils/constants');
+} = require("../utils/constants");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
-    .catch(() => res.status(SERVER_ERROR).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() =>
+      res.status(SERVER_ERROR).send({ message: "На сервере произошла ошибка" })
+    );
 };
 
 module.exports.getOneUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (user === null) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_ERROR).send({ message: "Пользователь не найден" });
       } else {
         res.status(200).send({ data: user });
       }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === "CastError") {
         res
           .status(INPUT_ERROR)
-          .send({ message: 'Переданы некорректные данные' });
+          .send({ message: "Переданы некорректные данные" });
       } else {
         res
           .status(SERVER_ERROR)
-          .send({ message: 'На сервере произошла ошибка' });
+          .send({ message: "На сервере произошла ошибка" });
       }
     });
 };
@@ -37,14 +39,14 @@ module.exports.createUser = (req, res) => {
   User.create(req.body)
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         res.status(INPUT_ERROR).send({
-          message: 'Переданы некорректные данные при создании пользователя',
+          message: "Переданы некорректные данные при создании пользователя",
         });
       } else {
         res
           .status(SERVER_ERROR)
-          .send({ message: 'На сервере произошла ошибка' });
+          .send({ message: "На сервере произошла ошибка" });
       }
     });
 };
@@ -56,20 +58,20 @@ module.exports.updateProfile = (req, res) => {
   })
     .then((user) => {
       if (!user) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_ERROR).send({ message: "Пользователь не найден" });
       } else {
         res.status(200).send({ data: user });
       }
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         res.status(INPUT_ERROR).send({
-          message: 'Переданы некорректные данные при обновлении профиля',
+          message: "Переданы некорректные данные при обновлении профиля",
         });
       } else {
         res
           .status(SERVER_ERROR)
-          .send({ message: 'На сервере произошла ошибка' });
+          .send({ message: "На сервере произошла ошибка" });
       }
     });
 };
@@ -81,20 +83,20 @@ module.exports.updateAvatar = (req, res) => {
   })
     .then((user) => {
       if (!user) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_ERROR).send({ message: "Пользователь не найден" });
       } else {
         res.status(200).send({ data: user });
       }
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         res.status(INPUT_ERROR).send({
-          message: 'Переданы некорректные данные при обновлении аватара',
+          message: "Переданы некорректные данные при обновлении аватара",
         });
       } else {
         res
           .status(SERVER_ERROR)
-          .send({ message: 'На сервере произошла ошибка' });
+          .send({ message: "На сервере произошла ошибка" });
       }
     });
 };
