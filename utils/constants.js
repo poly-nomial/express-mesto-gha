6bb32c0462d332d44b5210284e7f65adcae3b709
Lina Belitova <libra28@ya.ru>
2022-12-17 23:21:04 +0300

    Fix: can delete someone else's card

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,7 @@ const INPUT_ERROR = 400;
 const NOT_FOUND_ERROR = 404;
 const AUTHORIZATION_ERROR = 401;
 const CONFLICT_ERROR = 409;
+const FORBIDDEN_ERROR = 403;
 const URL_REGEX = /^https?:\/\/(www\.)?[\w\-._~:/?#[\]@!$&'()*+,;=]+$/;
 const ID_REGEX = /^[0-9a-f]{24}/;
 
@@ -12,6 +13,7 @@ module.exports = {
   NOT_FOUND_ERROR,
   AUTHORIZATION_ERROR,
   CONFLICT_ERROR,
+  FORBIDDEN_ERROR,
   URL_REGEX,
   ID_REGEX,
 };
