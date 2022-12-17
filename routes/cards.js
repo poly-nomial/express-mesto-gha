ace1a17398e1be6275357c76d57d0be13bcfe71a
Lina Belitova <libra28@ya.ru>
2022-12-17 22:29:32 +0300

    Fix: error (dis)liking card w/ nonexistent ID

--- routes/cards.js
+++ routes/cards.js
@@ -1,27 +1,35 @@
-const { celebrate, Joi } = require('celebrate');
-const cardRouter = require('express').Router();
+const { celebrate, Joi } = require("celebrate");
+const cardRouter = require("express").Router();
 const {
   getCards,
   createCard,
   deleteCard,
   likeCard,
   unlikeCard,
-} = require('../controllers/cards');
-const { URL_REGEX } = require('../utils/constants');
+} = require("../controllers/cards");
+const { URL_REGEX, ID_REGEX } = require("../utils/constants");
 
-cardRouter.get('/', getCards);
+cardRouter.get("/", getCards);
 cardRouter.post(
-  '/',
+  "/",
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().pattern(URL_REGEX),
     }),
   }),
-  createCard,
+  createCard
 );
-cardRouter.delete('/:cardId', deleteCard);
-cardRouter.put('/:cardId/likes', likeCard);
-cardRouter.delete('/:cardId/likes', unlikeCard);
+cardRouter.delete("/:cardId", deleteCard);
+cardRouter.put("/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(ID_REGEX);
+  })
+}), likeCard);
+cardRouter.delete("/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(ID_REGEX);
+  })
+}),unlikeCard);
 
 module.exports = cardRouter;
